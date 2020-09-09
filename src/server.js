0eb541576696e4b254522240b9ab137d855b77f5
Djonatan <Djonatanhorus@gmail.com>
2020-09-09 19:06:10 -0300

    y

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const boryparser = require('body-parser');
+const cors = require('cors');
 
 const porta = 3000;
 const app = express();
 
 app.use(boryparser.json());
+app.use(cors());
 
 app.use(require('./routers/router.js'));
 
